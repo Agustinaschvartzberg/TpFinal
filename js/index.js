15db7fe0f133bb2b3d740dbfa10503c48b61d06d
Victoria Stutzbach <victoriastutzbach@MacBook-Air-de-Victoria.local>
2023-11-10 09:22:16 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let apiKey= 'ba0b591fbb4dcbf21e7a279fceca5d5e'
+let apiKey= 'aa7bab2a0951387ff624d304c5d79062'
 fetch(`=${apiKey}`)
 .then (function (response){
     return response.json()
@@ -9,4 +9,25 @@ fetch(`=${apiKey}`)
 .catch(function(error){
     console.log(error)
 })
+
+
+
+
+// buscador
+
+function buscar() {
+    textInput = document.getElementsById ("#busqueda")
+    volordebusqueda= textInput.value()
+    window.location.href = "./serch-results.html?" + "HOLA" + valordebusqueda
+    URLSearchParams()
+}
+
+let botonBusqueda= document.getElementsById ("boton-buscar")
+botonBusqueda.addEventListner ("click",buscar)
+
+let contendor= document.querySelector('.contendor')
+
+for (let i=0 ; i<productos.data.lenght ; i++) {
+    contenedor.inner
+}
 
