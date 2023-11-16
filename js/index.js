48badb20ba3cec2ecc8113d86e44f5fa5d161b2b
Agustinaschvartzberg <142595355+Agustinaschvartzberg@users.noreply.github.com>
2023-11-16 17:04:59 -0300

    cambios index.js y modificaciones agus

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let keyy = "aa7bab2a0951387ff624d304c5d79062";
+let seriesPopulares = `https://api.themoviedb.org/3/tv/popular?api_key=${keyy}`;
+let peliculasMasPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${keyy}`;
+let peliculasMasVotadas = `https://api.themoviedb.org/3/movie/top_rated?api_key=${keyy}`;
+
+// Hacemos el fetch de las series populares
+fetch(seriesPopulares)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let contenedorSeries = document.querySelector(".Series");
+    let series = data.results;
+    for (let i = 0; i < 8; i++) {
+      contenedorSeries.innerHTML += `<li class="item"><a href="./detail-serie.html?id=${series[i].id}"><img src="https://image.tmdb.org/t/p/w500${series[i].poster_path}" alt="Poster">
+            <p>${series[i].name}</p>
+            <p>${series[i].first_air_date}</p>
+            </a></li>`;
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
